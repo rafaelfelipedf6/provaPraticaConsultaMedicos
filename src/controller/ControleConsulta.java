d691b38ea354da9ab8444daf61968c04b2e20d3b
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-14 11:26:03 -0300

    Adicionando Getters e Setters da classe ConsultaMedica e arrumando ControleConsulta com o nome da Classe correto

--- src/controller/ControleConsulta.java
+++ src/controller/ControleConsulta.java
@@ -3,25 +3,25 @@ import java.util.ArrayList;
 import model.*;
 
 public class ControleConsulta {
-	ArrayList<Consulta> listaConsultas;
+	ArrayList<ConsultaMedica> listaConsultas;
 	
 	public ControleConsulta(){
-		listaConsultas = new ArrayList<Consulta>();
+		listaConsultas = new ArrayList<ConsultaMedica>();
 	}
-	public void adicionar(Consulta umConsulta){
+	public void adicionar(ConsultaMedica umConsulta){
 		
 		listaConsultas.add(umConsulta);
 		
 	}
-	public void remover(Consulta umConsulta){
+	public void remover(ConsultaMedica umConsulta){
 		
 		listaConsultas.remove(umConsulta);
 	
 	}
-	public Consulta buscar(String umNome){
+	public ConsultaMedica buscar(String rg){
 		
-		for (Consulta umConsulta: listaConsultas){
-			if (umConsulta.getNome().equalsIgnoreCase(umNome)){
+		for (ConsultaMedica umConsulta: listaConsultas){
+			if (umConsulta.getPaciente().getRg().equals(rg)){
 				return umConsulta;
 			}
 		}
